c7041a858c3280a3921faee04b51b087d1057c17
Dylan Silverglate <dylansilverglate@Dylans-Air.lan>
2023-03-24 12:11:18 -0700

    added ejs files

--- Models/budget.js
+++ Models/budget.js
@@ -1,4 +1,4 @@
-module.exports = [
+ budget = [
     {
       date: "April 1",
       name: "Income",
@@ -70,3 +70,7 @@ module.exports = [
       tags: ["pets"]
     },
   ];
+
+  module.exports = {
+    budget,
+  }
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const methodOverride = require('method-override')
+
+// Models
+const models = require('./models/budget')
+const budget = models.budget
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send("Hello Adult Life")
+});
+
+app.get('/budgets', (req, res) => {
+    res.render('index.ejs', {budget: budget});
+});
+
+app.get('/budgets/:index', (req, res) => {
+    res.send("Part 1")
+});
+
+app.get('/budgets/new', (req, res) => {
+    res.send('new money time')
+});
+
+app.post('/budgets', (req, res) => {
+    res.send('post time')
+});
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000')
+});
